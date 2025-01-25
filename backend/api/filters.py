a05d20898b056faf9f367bc0267171e1120bfa13
“Elyar <ilyagurbanov96@gmail.com>
2025-01-25 12:49:13 +0400

    pervoye revyu

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+
 from recipes.models import Ingredient, Recipe, ShoppingCart, Tag
 
 
@@ -7,7 +8,7 @@ class IngredientFilter(django_filters.FilterSet):
 
     class Meta:
         model = Ingredient
-        fields = ['name', ]
+        fields = ('name',)
 
 
 class RecipeFilter(django_filters.FilterSet):
@@ -16,7 +17,6 @@ class RecipeFilter(django_filters.FilterSet):
         queryset=Tag.objects.all(),
         to_field_name='slug',
     )
-    author = django_filters.CharFilter(lookup_expr='exact')
     is_in_shopping_cart = django_filters.CharFilter(
         method='filter_is_in_shopping_cart')
     is_favorited = django_filters.CharFilter(method='filter_is_favorited')
@@ -27,20 +27,13 @@ class RecipeFilter(django_filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
-        if not user.is_authenticated:
-            return queryset.none()
-        if str(value).lower() in ['1', 'true']:
+        if value:
             return queryset.filter(favorites__user=user)
-        else:
-            return queryset.exclude(favorites__user=user)
+        return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
-        if not user.is_authenticated:
-            return queryset.none()
-        shopping_cart_recipes = ShoppingCart.objects.filter(
-            user=user).values_list('recipe', flat=True)
-        if str(value).lower() in ['1', 'true']:
+        shopping_cart_recipes = ShoppingCart.objects.filter(user=user).values_list('recipe', flat=True)
+        if value:
             return queryset.filter(id__in=shopping_cart_recipes)
-        else:
-            return queryset.exclude(id__in=shopping_cart_recipes)
+        return queryset
